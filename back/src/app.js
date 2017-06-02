91e58c6224ece6b6feab40197c86173ab01d5749
Yann Carlen <yann.carlen@epitech.eu>
2017-06-02 19:36:32 +0200

    add socket deps

--- back/src/app.js
+++ back/src/app.js
@@ -8,6 +8,9 @@ require('dotenv').config()
 
 const app = express()
 
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
 const handleError = err => {
     console.error(err)
     process.exit(1)
