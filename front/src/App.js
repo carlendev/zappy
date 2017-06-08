a7af21d2693cd8963059db57edab7174da62ca46
Quentin Muller <quent1.mullerpro@gmail.com>
2017-06-08 14:11:05 +0200

    Change Folder

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
 import { Loop, Stage, TileMap } from 'react-game-kit';
 
-
 class App extends Component {
-  render() {
+
+render() {
     return (
       <Loop>
-          <Stage>
-              <TileMap
-                  src="../../assets/MegaSprite.png"
-                  tileSize={128}
-                  columns={24}
-                  rows={4}
-                  layers={[
+        <Stage width={1024} height={576}>
+            <TileMap
+                src="../assets/Background.jpg"
+                tileSize={128}
+                columns={24}
+                rows={4}
+                layers={[
                     [
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                     ],
-                  ]}
-                />
-          </Stage>
+                ]}
+            />
+        </Stage>
       </Loop>
-    );
+    )
   }
 }
 
