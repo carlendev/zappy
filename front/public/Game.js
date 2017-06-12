907c8936398eee87cd3203f96dacf76b5e63b54d
Yann Carlen <yann.carlen@epitech.eu>
2017-06-12 16:12:28 +0200

    improve code quality

--- front/public/Game.js
+++ front/public/Game.js
@@ -1,10 +1,13 @@
 window.onload = () => {
-    //start crafty
-    var _this;
 
-    Crafty.init(400,320, document.getElementById('game'));
+    const tileMapSize = 16
+    const size = 50
+    const windowSize = 50 * tileMapSize
+    
+    Crafty.init(windowSize, windowSize, document.getElementById('game'))
+    
     //turn the sprite map into usable components
-    Crafty.sprite(16, "/images/sprite.png", {
+    Crafty.sprite(16, '/images/sprite.png', {
         grass1: [0,0],
         grass2: [1,0],
         grass3: [2,0],
@@ -13,96 +16,71 @@ window.onload = () => {
         bush1:  [0,2],
         bush2:  [1,2],
         player: [0,3]
-    });
+    })
 
     //method to randomy generate the map
-    function generateWorld() {
+    const generateWorld = () => {
         //generate the grass along the x-axis
-        for(var i = 0; i < 25; i++) {
+        for(let i = 0; i < size; ++i) {
             //generate the grass along the y-axis
-            for(var j = 0; j < 20; j++) {
-                grassType = Math.floor((Math.random() * 4) + 1);
-                Crafty.e("2D, Canvas, grass"+grassType)
-                    .attr({x: i * 16, y: j * 16});
-                
-                //1/50 chance of drawing a flower and only within the bushes
-                if(i > 0 && i < 24 && j > 0 && j < 19 && (Math.floor((Math.random() * 50) + 0)) > 49) {
-                    Crafty.e("2D, DOM, flower, SpriteAnimation")
-                        .attr({x: i * 16, y: j * 16})
-                        .animate("wind", 0, 1, 3)
-                        .bind("enterframe", function() {
-                            if(!this.isPlaying())
-                                this.animate("wind", 80);
-                        });
-                }
+            for(let j = 0; j < size; ++j) {
+                grassType = Math.floor((Math.random() * 4) + 1)
+                Crafty.e('2D, Canvas, grass'+grassType).attr({ x: i * tileMapSize, y: j * tileMapSize })
             }
-        }
+        }   
         
         //create the bushes along the x-axis which will form the boundaries
-        for(var i = 0; i < 25; i++) {
-            Crafty.e("2D, Canvas, wall_top, bush"+Math.floor((Math.random() * 2) + 1))
-                .attr({x: i * 16, y: 0, z: 2});
-            Crafty.e("2D, DOM, wall_bottom, bush"+Math.floor((Math.random() * 2) + 1))
-                .attr({x: i * 16, y: 304, z: 2});
+        for(let i = 0; i < size; ++i) {
+            Crafty.e('2D, Canvas, wall_top, bush' + Math.floor((Math.random() * 2) + 1))
+                .attr({ x: i * tileMapSize, y: 0, z: 2 });
+            Crafty.e('2D, DOM, wall_bottom, bush' + Math.floor((Math.random() * 2) + 1))
+                .attr({ x: i * tileMapSize, y: (size - 1) * tileMapSize, z: 2 });
         }
         
         //create the bushes along the y-axis
         //we need to start one more and one less to not overlap the previous bushes
-        for(var i = 1; i < 19; i++) {
-            Crafty.e("2D, DOM, wall_left, bush"+Math.floor((Math.random() * 2) + 1))
-                .attr({x: 0, y: i * 16, z: 2});
-            Crafty.e("2D, Canvas, wall_right, bush"+Math.floor((Math.random() * 2) + 1))
-                .attr({x: 384, y: i * 16, z: 2});
+        for(let i = 1; i < size; i++) {
+            Crafty.e('2D, DOM, wall_left, bush' + Math.floor((Math.random() * 2) + 1))
+                .attr({ x: 0, y: i * tileMapSize, z: 2 });
+            Crafty.e('2D, Canvas, wall_right, bush' + Math.floor((Math.random() * 2) + 1))
+                .attr({ x: (size - 1) * tileMapSize, y: i * tileMapSize, z: 2 });
         }
     }
 
     //the loading screen that will display while our assets load
-    Crafty.scene("loading", function() {
+    Crafty.scene('loading', () => {
         //load takes an array of assets and a callback when complete
-        const sprite =  {
-            "sprites": {
-                "/images/sprite.png": {}
-            }
-        }
-        Crafty.load(sprite, function() {
-            Crafty.scene("main"); //when everything is loaded, run the main scene
-        });
+        Crafty.scene('main') //when everything is loaded, run the main scene
         
         //black background with some loading text
-        Crafty.background("#000");
-        Crafty.e("2D, DOM, Text").attr({w: 100, h: 20, x: 150, y: 120})
-            .text("Loading")
-            .css({"text-align": "center"});
-    });
+        Crafty.background('#000')
+        Crafty.e('2D, DOM, Text').attr({ w: 100, h: 20, x: 150, y: 120 })
+            .text('Loading')
+            .css({ 'text-align': 'center' })
+    })
 
-    Crafty.scene("main", function() {
-        generateWorld();
+    Crafty.scene('main', () => {
+        Crafty.background('#000')
+        generateWorld()
         
-        Crafty.e("2D, Canvas, player, Controls, Collision")
-            .attr({x: 10, y: 10, w: 30, h: 30})
+        Crafty.e('2D, Canvas, player, Controls, Collision')
+            .attr({ x: 10, y: 10, w: 30, h: 30 })
             .bind('KeyDown', function(e) {
-                if(e.key == Crafty.keys.LEFT_ARROW) {
-                this.x = this.x - 16;
-                } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-                this.x = this.x + 16;
-                } else if (e.key == Crafty.keys.UP_ARROW) {
-                this.y = this.y - 16;
-                } else if (e.key == Crafty.keys.DOWN_ARROW) {
-                this.y = this.y + 16;
-                }
+                if(e.key === Crafty.keys.LEFT_ARROW) this.x -= tileMapSize
+                else if (e.key === Crafty.keys.RIGHT_ARROW) this.x += tileMapSize
+                else if (e.key === Crafty.keys.UP_ARROW) this.y -= tileMapSize
+                else if (e.key === Crafty.keys.DOWN_ARROW) this.y += tileMapSize
             })
             .collision()
-                .onHit("wall_left", function() {
-                    this.x += 16;
-                }).onHit("wall_right", function() {
-                    this.x -= 16;
-                }).onHit("wall_bottom", function() {
-                    this.y -= 16;
-                }).onHit("wall_top", function() {
-                    this.y += 16;
-                });
-});
-
-    //automatically play the loading scene
-    Crafty.scene("loading");
+            .onHit('wall_left', function() {
+                this.x += tileMapSize
+            }).onHit('wall_right', function() {
+                this.x -= tileMapSize
+            }).onHit('wall_bottom', function() {
+                this.y -= tileMapSizex
+            }).onHit('wall_top', function() {
+                this.y += tileMapSize
+            })
+})
+    Crafty.scene('loading')
 }
