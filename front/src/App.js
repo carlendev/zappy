c455c19ff99830165bcc4bc779e8af33eadd641c
ludovic gueth <ludovicgueth@gmail.com>
2017-06-21 09:31:21 +0200

    correct front

--- front/src/App.js
+++ front/src/App.js
@@ -3,16 +3,11 @@ import Game from "./Game";
 import Data from "./Data";
 
 class App extends Component {
-  nextStep() {
-    document.getElementById("game").style.display = "block";
-    document.getElementById("data").style.display = "none";
-  }
-
   render() {
     return (
       <div>
         <div id="data" className="container">
-          <Data nextStep={this.nextStep} />
+          <Data />
         </div>
 
         <Game />
--- front/src/Data.js
+++ front/src/Data.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 
 export default class Data extends Component {
+  constructor(props) {
+    super(props);
+    this.firstStep = this.firstStep.bind(this);
+  }
   state = {};
+
+  firstStep(e) {
+    e.preventDefault();
+    document.getElementById("game").style.display = "block";
+    document.getElementById("data").style.display = "none";
+  }
+
   render() {
     return (
       <form>
+
         <div className="field">
           <label className="label">Name</label>
           <p className="control">
@@ -12,7 +24,7 @@ export default class Data extends Component {
           </p>
         </div>
 
-        <button className="button is-info" onClick={this.nextStep}>
+        <button className="button is-info" onClick={this.firstStep}>
           Jouer
         </button>
 
