47150efc25748bb633a60798404490a5924ba7d1
Yann Carlen <yann.carlen@epitech.eu>
2017-06-24 20:57:23 +0200

    done client refactor

--- back/src/socket/client/index.js
+++ back/src/socket/client/index.js
@@ -2,36 +2,27 @@ const { logInfoSocket, logQInfo, logQError } = require('../../utils/logger')
 const { clientPnw, validateJson } = require('../../utils/validator')
 const { createHub, deleteHub } = require('../hub/index')
 const { connectFront } = require('../front/index')
-const { set, get } = require('../../utils/redisfn')
+const { set, get, findClients, findHubs, setClients } = require('../../utils/redisfn')
 const { createHubQ, createHubJob } = require('../../queue/index')
 const { randTile } = require('../../utils/map')
 
 let _clients = {}
 
-const findClients = id => new Promise(s => get('clients').then(e => {
-    const _clients = JSON.parse(e)
-    s([ _clients, _clients.find(c => c.id === id) ])
-}))
-
-const findHubs = hubName => new Promise(s => get('hubs').then(e => {
-    _hubs = JSON.parse(e)
-    s([ _hubs, _hubs.find(h => h.hubName === hubName) ])
-}))
-
-const setClients = (clients, fn, ...args) => set('clients', JSON.stringify(clients)).then(() => fn(...args))
+const emitDead = (client, msg) => {
+    logInfoSocket(msg)
+    client.emit('dead')
+}
 
 const registerClient = (clients, client, data, nbTeam, nbPlayerMax, playerPos, io) => {
     clients[ client.id ] = { socket: client, id: client.id, front: false, hub: data.hubName, team: data.team }
     _clients = clients
     logInfoSocket('Client connected ' + client.id)
-    get('clients').then(e => {
-        const add = JSON.parse(e)
+    findClients('').then(([ add ]) => {
         const id = client.id
         add.push(Object.assign(data, { id, pos: playerPos, orientation: 1 }))
         set('clients', JSON.stringify(add)).then(e => {
             createHubQ(id, userEvents)
-            get('clients').then(e => {
-                const _clients = JSON.parse(e)
+            findClients('').then(([ _clients ]) => {
                 const playerInHub = _clients.filter(e => e.hubName === data.hubName)
                 if (playerInHub.length !== nbPlayerMax * nbTeam) return 
                 io.emit('play')
@@ -44,28 +35,11 @@ const registerClient = (clients, client, data, nbTeam, nbPlayerMax, playerPos, i
 }
 
 const connect = (data, clients, client, io) => {
-    if (validateJson(clientPnw)(data).errors.length) {
-        client.emit('dead')
-        return
-    }
-    get('hubs').then(async e => {
-        const _hubs = JSON.parse(e)
-        if (!_hubs.length) {
-            logInfoSocket('Connection rejected, no hubs found')
-            client.emit('dead')
-            return
-        }
-        if (!_hubs.find(e => e.hubName === data.hubName)) {
-            logInfoSocket(`Connection rejected, ${data.hubName} hub not found`)
-            client.emit('dead')
-            return
-        }
-        const currentHub = _hubs.find(e => e.hubName === data.hubName)
-        if (!currentHub.teams.find(e => e === data.team)) {
-            logInfoSocket(`Connection rejected, ${data.team} team not found`)
-            client.emit('dead')
-            return
-        }
+    if (validateJson(clientPnw)(data).errors.length) return emitDead(client, 'Wrong parameters types')
+    findHubs(data.hubName).then(async ([ _hubs, currentHub ]) => {
+        if (!_hubs.length) return emitDead(client, 'Connection rejected, no hubs found')
+        if (!_hubs.find(e => e.hubName === data.hubName)) return emitDead(client,`Connection rejected, ${data.hubName} hub not found`)
+        if (!currentHub.teams.find(e => e === data.team)) return emitDead(client, `Connection rejected, ${data.team} team not found`)
         const team = _hubs.find(e => e.team === data.team)
         const nbTeam = currentHub.teams.length
         const nbPlayerMax = +currentHub.clientsPerTeam
@@ -74,18 +48,10 @@ const connect = (data, clients, client, io) => {
         const playerPos = randTile(currentHub.mapWidth, currentHub.mapHeight)
         if (!playerInHub.length) return registerClient(clients, client, data, nbTeam, nbPlayerMax, playerPos, io)
         const nbPlayer = playerInHub.length
-        if (nbPlayer === nbPlayerMax * nbTeam) {
-            logInfoSocket(`Connection rejected, ${data.hubName} to much player in this hub`)
-            client.emit('dead')
-            return                    
-        }
+        if (nbPlayer === nbPlayerMax * nbTeam) return emitDead(client, `Connection rejected, ${data.hubName} to much player in this hub`)
         const playerInTeam = playerInHub.find(e => e.team === data.team)
         if (playerInTeam === undefined || playerInTeam === null) return registerClient(clients, client, data, nbTeam, nbPlayerMax, playerPos, io)
-        if (playerInTeam.length === currentHub.nbPlayerMax) {
-            logInfoSocket(`Connection rejected, ${data.hubName} to much player in this team`)
-            client.emit('dead')
-            return
-        }
+        if (playerInTeam.length === currentHub.nbPlayerMax) return emitDead(client, `Connection rejected, ${data.hubName} to much player in this team`)
         return registerClient(clients, client, data, nbTeam, nbPlayerMax, io)
     })
 }
@@ -109,18 +75,18 @@ const disconnect = (data, clients, client) => {
 const forward = (data, clients, client) => findClients(client.id).then(([ _clients, _client ]) =>
     findHubs(_client.hubName).then(([ _hubs, _hub ]) => {
         switch (_client.orientation) {
-            case 1:
-                if (--_client.pos.y < 0) _client.pos.y = _hub.mapHeight - 1
-                break
-            case 2:
-                if (--_client.pos.x) _client.pos.x = _hub.mapWidth - 1
-                break
-            case 3:
-                _client.pos.y = (_client.pos.y + 1) % _hub.mapHeight
-                break
-            case 4:
-                _client.pos.x = (_client.pos.x + 1) % _hub.mapWidth
-                break
+        case 1:
+            if (--_client.pos.y < 0) _client.pos.y = _hub.mapHeight - 1
+            break
+        case 2:
+            if (--_client.pos.x) _client.pos.x = _hub.mapWidth - 1
+            break
+        case 3:
+            _client.pos.y = (_client.pos.y + 1) % _hub.mapHeight
+            break
+        case 4:
+            _client.pos.x = (_client.pos.x + 1) % _hub.mapWidth
+            break
         }
     setClients(_clients, _client => logInfoSocket('New pos is: ' + JSON.stringify(_client.pos)), _client)
 }))
--- back/src/utils/redisfn.js
+++ back/src/utils/redisfn.js
@@ -6,4 +6,16 @@ const set = (key, value) => redis.setAsync(key, value)
 
 const send = cmd => redis.send_commandAsync(cmd)
 
-module.exports = { get, set, send }
+const findClients = id => new Promise(s => get('clients').then(e => {
+    const _clients = JSON.parse(e)
+    s([ _clients, _clients.find(c => c.id === id) ])
+}))
+
+const findHubs = hubName => new Promise(s => get('hubs').then(e => {
+    _hubs = JSON.parse(e)
+    s([ _hubs, _hubs.find(h => h.hubName === hubName) ])
+}))
+
+const setClients = (clients, fn, ...args) => set('clients', JSON.stringify(clients)).then(() => fn(...args))
+
+module.exports = { get, set, send, findClients, findHubs, setClients }
