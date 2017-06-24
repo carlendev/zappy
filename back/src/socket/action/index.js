e064c6c45ac8c31dd261f8fbe60cfea0e87c1bcd
Yann Carlen <yann.carlen@epitech.eu>
2017-06-24 18:34:21 +0200

    refactor action code

--- back/src/socket/action/index.js
+++ back/src/socket/action/index.js
@@ -4,55 +4,26 @@ const { createHubJob } = require('../../queue/index')
 
 const getFrontId = clients => Object.keys(clients).find(e => clients[e].front === true)
 
-const moveForward = (data, clients, client, hubs) => {
-  get('clients').then(e => {
-    const _clients = JSON.parse(e)
-    const _client = _clients.find(c => c.id === client.id)
-    const front_id = getFrontId(clients)
-    createHubJob(_client.id, {
-      client_id: _client.id,
-      front_id: front_id,
-      hub: _client.hub,
-      id: 'Forward',
-      title: 'Forward',
-      fn: 'forward',
-      time: 1
-    }, () => console.log('Forward queued'))
-  })
+const getClientById = (clients, id) => clients.find(c => c.id === id)
+
+const forgeClientInfo = (client, front_id) => ({ client_id: client.id, front_id, hub: client.hub })
+
+const forgeInfoString = (action, time) => ({ id: action, title: action, fn: action.toLowerCase(), time })
+
+const forge = (e, clientsLocal, id) => {
+  const clients = JSON.parse(e)
+  const front_id = getFrontId(clientsLocal)
+  const client = getClientById(clients, id)
+  return (action, time) => Object.assign(forgeClientInfo(client, front_id), forgeInfoString(action, time))
 }
 
-const moveRight = (data, clients, client, hubs) => {
-  get('clients').then(e => {
-    const _clients = JSON.parse(e)
-    const _client = _clients.find(c => c.id === client.id)
-    const front_id = getFrontId(clients)
-    createHubJob(_client.id, {
-      client_id: _client.id,
-      front_id: front_id,
-      hub: _client.hub,
-      id: 'Right',
-      title: 'Right',
-      fn: 'right',
-      time: 1
-    }, () => console.log('Right queued'))
-  })
-}
+const moveForward = (data, clientsLocal, client, hubs) => 
+  get('clients').then(e => createHubJob(client.id, forge(e, clientsLocal, client.id)('Forward', 1), () => console.log('Forward queued')))
 
-const moveLeft = (data, clients, client, hubs) => {
-  get('clients').then(e => {
-    const _clients = JSON.parse(e)
-    const _client = _clients.find(c => c.id === client.id)
-    const front_id = getFrontId(clients)
-    createHubJob(_client.id, {
-      client_id: _client.id,
-      front_id: front_id,
-      hub: _client.hub,
-      id: 'Left',
-      title: 'Left',
-      fn: 'left',
-      time: 1
-    }, () => console.log('Left queued'))
-  })
-}
+const moveRight = (data, clientsLocal, client, hubs) =>
+  get('clients').then(e => createHubJob(client.id, forge(e, clientsLocal, client.id)('Right', 1), () => console.log('Right queued')))
+
+const moveLeft = (data, clientsLocal, client, hubs) =>
+  get('clients').then(e => createHubJob(client.id, forge(e, clientsLocal, client.id)('Left', 1), () => console.log('Left queued')))
 
 module.exports = { moveForward, moveRight, moveLeft }
