6b1dc49233be6bce10bde086084a3573f99eb431
Guillaume HITIER <guillaume.hitier@epitech.eu>
2017-07-01 01:32:11 +0200

    added ia packages and removed unused client folder

--- ia/src/app.js
+++ ia/src/app.js
@@ -1,1 +1,31 @@
-console.log('--------------------------------- New Client ---------------------------------')
+const socket = require('socket.io-client')
+const argv = require('argv')
+
+const args = argv.option([
+    {
+        name: 'hub',
+        type: 'string',
+        description: 'Name of the hub',
+        example: "./app --hub=\"hub1\" --team=\"BITE\" --address=\"http://127.0.0.1:3001\""
+    },
+    {
+        name: 'team',
+        type: 'string',
+        description: 'Name of the team',
+        example: "./app --hub=\"hub1\"  --team=\"ISSOU\" --address=\"http://127.0.0.1:3001\""
+    },
+    {
+        name: 'address',
+        type: 'string',
+        description: 'Address of the server',
+        example: "./app --hub=\"hub1\"  --team=\"ISSOU\" --address=\"http://127.0.0.1:3001\""
+    },
+]).run()
+
+const io = socket.connect(args.options.address)
+
+io.on('connect', () => {
+    io.emit('join', { hubName: args.options.hub, team: args.options.team })
+})
+
+const fuckQ = []
