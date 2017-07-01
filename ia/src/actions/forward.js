b8db9f18bf6071ae7a0059bd112ec00b1dd3764d
Guillaume HITIER <guillaume.hitier@epitech.eu>
2017-07-01 19:42:22 +0200

    cleaned actions

--- ia/src/actions/forward.js
+++ ia/src/actions/forward.js
@@ -1,11 +1,14 @@
 const { io } = require('../app')
 const { emit } = require('../emit')
 
+const k = () => console.log('Forward ok')
+
 function forward() {
   console.log('Forward')
-  emit('Forward')
-  emit('Forward')
-  emit('Forward')
+  emit('Forward', k)
+  emit('Forward', k)
+  emit('Forward', k)
+  emit('Take', () => console.log('Took bite'), { object: 'bite' })
 }
 
 module.exports = { forward }
--- ia/src/start.js
+++ ia/src/start.js
@@ -1,18 +1,13 @@
 const { io } = require('./app')
+const { action, nextAction } = require('./action')
 const { look } = require('./actions/look')
 const { forward } = require('./actions/forward')
 
-const actionQ = []
 const objectiveQ = []
-
-function nextAction() {
-    const action = actionQ.shift()
-    action && action()
-}
 
 function start() {
     console.log('starting to play')
-    actionQ.push(forward)
+    action(forward)
     nextAction()
 }
 
